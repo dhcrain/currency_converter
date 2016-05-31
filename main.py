d0e4c9a21a27ae3258fcd956a77f5914f6a88040
dhcrain <dhcrain@gmail.com>
2016-05-31 17:01:57 -0400

    Hard mode done

--- main.py
+++ main.py
@@ -6,69 +6,69 @@ class Money:
         self.amount = amount
         self.currency = currency
 
-# Implement the conversion rates for USD, JPY, EUR, and BTC.
-# 1 usd = 110.74 JPY / JPY to usd 0.009031
-# 1 usd = 0.90 EUR / eur to usd 1.113634
-# 1 usd = 0.0019 BTC / btc to usd 529.72
-
-    def to_usd(self, amount, currency):
+    @property
+    def to_usd(self):
         jpy_usd = 0.009031
         eur_usd = 1.113634
         btc_usd = 529.72
-        if currency == "JPY":
-            return amount * jpy_usd
-        elif currency == "EUR":
-            return amount * eur_usd
-        elif currency == "BTC":
-            return amount * btc_usd
-        else:
-            return amount
+        if self.currency == "JPY":
+            return self.amount * jpy_usd
+        elif self.currency == "EUR":
+            return self.amount * eur_usd
+        elif self.currency == "BTC":
+            return self.amount * btc_usd
+        else:   # USD
+            return self.amount
 
     def __str__(self):
         return "{} {}".format(self.amount, self.currency.upper())
 
     def __eq__(self, other):
-        return self.to_usd(self.amount, self.currency) == other.to_usd(other.amount, other.currency)
+        return self.to_usd == other.to_usd
 
     def __ne__(self, other):
-        return self.to_usd(self.amount, self.currency) != other.to_usd(other.amount, other.currency)
+        return self.to_usd != other.to_usd
 
     def __le__(self, other):
-        return self.to_usd(self.amount, self.currency) <= other.to_usd(other.amount, other.currency)
+        return self.to_usd <= other.to_usd
 
     def __lt__(self, other):
-        return self.to_usd(self.amount, self.currency) < other.to_usd(other.amount, other.currency)
+        return self.to_usd < other.to_usd
 
     def __gt__(self, other):
-        return self.to_usd(self.amount, self.currency) > other.to_usd(other.amount, other.currency)
+        return self.to_usd > other.to_usd
 
     def __ge__(self, other):
-        return self.to_usd(self.amount, self.currency) >= other.to_usd(other.amount, other.currency)
+        return self.to_usd >= other.to_usd
 
     def __add__(self, other):
-        return self.to_usd(self.amount, self.currency) + other.to_usd(other.amount, other.currency)
+        return Money((self.to_usd + other.to_usd), self.currency)
 
     def __mul__(self, other):
-        return self.to_usd(self.amount, self.currency) * other.to_usd(other.amount, other.currency)
+        return Money((self.to_usd * other.to_usd), self.currency)
 
     def __sub__(self, other):
-        return self.to_usd(self.amount, self.currency) - other.to_usd(other.amount, other.currency)
+        return Money((self.to_usd - other.to_usd), self.currency)
 
 
-btc = Money(11, "BTC")
-eur = Money(333, "EUR")
-jpy = Money(20000, "YPY")
-usd = Money(100, "USD")
+btc = Money(1, "BTC")
+eur = Money(1, "EUR")
+jpy = Money(1, "JPY")
+usd = Money(1, "USD")
 
-print("All Results in USD")
-print("BTC - USD:", btc - usd)
-print("EUR * JPY:", eur * jpy)
-print("EUR + BTC:", eur + btc)
-print("BTC > USD:", btc > usd)
->=print(EUR )
-<
-<=
-!=
-==
+# print("All Results in USD")
+# print("BTC - USD:", btc - usd)
+# print("EUR * JPY:", eur * jpy)
+# print("EUR + BTC:", eur + btc)
+# print("BTC > USD:", btc > usd)
+# print("EUR >= JPY", eur >= jpy)
+# print("JPY < BTC", jpy < btc)
+
+print(btc.to_usd)
+print(eur.to_usd)
+print(jpy.to_usd)
+print(usd.to_usd)
+print(Money(100.00, "USD") + Money(56.32, "EUR") + Money(1.2, "BTC") + Money(8, "USD"))
 
 
+print(100 + 62.70 + 625.98 + 8)
